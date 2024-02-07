8bcf5a9f0beae29aa86c09af29430420e1ea3677
Guillaume JIBAUT <guillaume.jibaut@konicaminolta.fr>
2024-02-07 15:50:30 +0100

    Added Model library

---
+++ Caeruleum.Model/Plant.cs
@@ -0,0 +1,13 @@
+﻿namespace Caeruleum.Model;
+
+public record Plant(
+    int Id,
+    string Name,
+    string ScienticName,
+    string Category
+)
+{
+    public Plant()
+        : this(0, string.Empty, string.Empty, string.Empty)
+    { }
+}
---
+++ Caeruleum.Model/Service/IPlantService.cs
@@ -0,0 +1,7 @@
+﻿namespace Caeruleum.Model.Service;
+
+public interface IPlantService
+{
+    IAsyncEnumerable<Plant> GetAllAsync();
+    ValueTask<Plant?> GetByIdAsync(int plantId);
+}
