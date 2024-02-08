1a9b112de93841a36bad17576f7d6ab39390787d
Guillaume JIBAUT <guillaume.jibaut@konicaminolta.fr>
2024-02-08 09:35:13 +0100

    MAJ InMemoryDb

--- Caeruleum.InMemoryDb/InMemoryPlants.cs
+++ Caeruleum.InMemoryDb/InMemoryPlants.cs
@@ -11,6 +11,10 @@ internal static class InMemoryPlants
         new(3, "Centaurée bleuet", "Centaurea cyanus", "Astéracées"),
         new(4, "Digitale pourpre", "Digitalis purpurea", "Plantaginacées"),
         new(5, "Estragon", "Artemisia dracunculus", "Astéracées"),
+        new(6, "Verveine", "Aloysia citriodora", "Verbénacées"),
+        new(7, "Mélisse", "Melissa officinalis", "Lamiacées"),
+        new(8, "Rosier des chiens", "Rosa canina", "Rosacées"),
+        new(9, "Raifort", "Armoracia rusticana", "Brassicacées"),
     ];
 
     public static Plant? GetById(int plantId)
