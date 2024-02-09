a0a9678fe56d2ea595068756c9cddac845a0d30b
Guillaume JIBAUT <guillaume.jibaut@konicaminolta.fr>
2024-02-09 09:01:58 +0100

    Added API

---
+++ Caeruleum.Api/Controllers/PlantController.cs
@@ -0,0 +1,35 @@
+﻿using Caeruleum.Model;
+using Caeruleum.Model.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caeruleum.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlantController(
+    ILogger<PlantController> logger,
+    IPlantService plantService
+) : ControllerBase
+{
+    [HttpGet]
+    [Route($"{nameof(All)}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IAsyncEnumerable<Plant> All()
+    {
+        logger.LogDebug("Get all plants");
+        return plantService.GetAllAsync();
+    }
+
+    /*[HttpGet]
+    [Route($"{nameof(plantId)}:int:min(1)")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Plant>> GetOne([FromRoute] int plantId)
+    {
+        logger.LogDebug("Try get plant with id = {plantId}", plantId);
+
+        return await plantService.GetByIdAsync(plantId) is Plant plant
+            ? Ok(plant)
+            : NotFound();
+    }*/
+}
---
+++ Caeruleum.Api/Program.cs
@@ -0,0 +1,28 @@
+using Caeruleum.InMemoryDb.Extensions;
+using Serilog;
+using Serilog.Events;
+
+string logTemplate = "{Timestamp:HH:mm:ss}|{Level:u4}|{SourceContext}|{Message:lj}{NewLine}{Exception}";
+
+var builder = WebApplication.CreateBuilder(args);
+IServiceCollection services = builder.Services;
+services.AddSerilog(loggerCfg =>
+{
+    loggerCfg.WriteTo.Console(outputTemplate: logTemplate);
+    loggerCfg.MinimumLevel.Is(LogEventLevel.Information);
+    loggerCfg.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+    loggerCfg.MinimumLevel.Override("System", LogEventLevel.Warning);
+    loggerCfg.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information);
+});
+services.AddCaeruleumInMemoryDb();
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
